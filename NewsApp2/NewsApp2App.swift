5960b19eb5a39f16f8996f8c2189727fbc6765d7
Jin Park <jinpark1504@gmail.com>
2024-06-21 15:13:36 +0900

    Initial Commit

---
+++ NewsApp2/NewsApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp2App.swift
+//  NewsApp2
+//
+//  Created by Jungjin Park on 2024-06-21.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
