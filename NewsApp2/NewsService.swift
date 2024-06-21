d91cd982a366a4002b5f17c4db2ac5fe2c6b1063
Jin Park <jinpark1504@gmail.com>
2024-06-21 16:25:10 +0900

    Create NewsService

---
+++ NewsApp2/NewsService.swift
@@ -0,0 +1,49 @@
+//
+//  NewsService.swift
+//  NewsApp2
+//
+//  Created by Jungjin Park on 2024-06-21.
+//
+
+import Foundation
+import Combine
+
+struct NewsService {
+    private let baseURL = "https://openapi.naver.com/v1/search/news.json"
+    private let clientID: String
+    private let clientSecret: String
+    
+    init() {
+        guard let clientID = Bundle.main.object(forInfoDictionaryKey: "CLIENT_ID") as? String,
+              let clientSecret = Bundle.main.object(forInfoDictionaryKey: "CLIENT_SECRET") as? String else {
+            fatalError("CLIENT_ID or CLIENT_SECRET not set in plist")
+        }
+        self.clientID = clientID
+        self.clientSecret = clientSecret
+    }
+    
+    func searchNews(query: String, page: Int, itemsPerPage: Int) -> AnyPublisher<NewsResponse, Error> {
+        guard var components = URLComponents(string: baseURL) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "start", value: String((page - 1) * itemsPerPage + 1)),
+            URLQueryItem(name: "display", value: String(itemsPerPage)),
+        ]
+        
+        guard let url = components.url else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue(clientID, forHTTPHeaderField: "X-Naver-Client-Id")
+        request.addValue(clientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: NewsResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
